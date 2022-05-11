c714f32f1cffe3595fc7766ab72a1fb887dc0747
Sushant Sahu <sushant87@gmail.com>
2022-05-11 12:37:15 +0530

    feat: added transactions

--- db/entrypoint-initdb.sql
+++ db/entrypoint-initdb.sql
@@ -1,12 +1,17 @@
-CREATE TABLE User (
+CREATE TABLE Transaction (
     ID INT AUTO_INCREMENT, 
-    Name VARCHAR(50) NOT NULL, 
-    Gender VARCHAR(1) NOT NULL,
+    Description VARCHAR(50) NOT NULL, 
+    Price INT NOT NULL,
+    isDeleted BIT DEFAULT 0, 
     PRIMARY KEY(ID)    
 );
 
-ALTER TABLE User AUTO_INCREMENT=100;
+ALTER TABLE Transaction AUTO_INCREMENT=100;
 
-INSERT INTO User(Name, Gender) VALUES ('Sushant', 'M');
-INSERT INTO User(Name, Gender) VALUES ('Khushboo', 'F');
-INSERT INTO User(Name, Gender) VALUES ('Saurav', 'M');
+INSERT INTO Transaction(Description, Price) VALUES ('Receipt 1', 11.22);
+INSERT INTO Transaction(Description, Price) VALUES ('Receipt 2', 22.12);
+INSERT INTO Transaction(Description, Price) VALUES ('Receipt 3', 9.80);
+INSERT INTO Transaction(Description, Price) VALUES ('Receipt 4', 19.90);
+INSERT INTO Transaction(Description, Price) VALUES ('Receipt 5', 22.11);
+INSERT INTO Transaction(Description, Price) VALUES ('Receipt 6', 33.90);
+INSERT INTO Transaction(Description, Price) VALUES ('Receipt 7', 11.11);
